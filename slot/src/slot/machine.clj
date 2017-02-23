8a3d1ef9bf38b414c156b83c33bae20a615fcb5c
hjchoi <hjchoi@labfelix.com>
2017-02-23 22:49:26 +0900

    a

--- slot/src/slot/machine.clj
+++ slot/src/slot/machine.clj
@@ -22,15 +22,42 @@
         주된 행위는 이게 다인듯
   )
 
-(def reelstrip [[0 1 2 3 4 5 6 7 8 9 10 11 12 13]
-                      [0 1 2 3 4 5 6 7 8 9 10 11 12 13]
-                      [0 1 2 3 4 5 6 7 8 9 10 11 12 13]
-                      [0 1 2 3 4 5 6 7 8 9 10 11 12 13]
-                      [0 1 2 3 4 5 6 7 8 9 10 11 12 13]])
+(def symbols {
+               :w1 0
+               :s1 1
+               :s2 2
+               :s3 3
+               :s4 4
+               :s5 5
+               :a 6
+               :k 7
+               :q 8
+               :j 9
+               :bs 10
+                })
+
+(def paylines [[1 1 1 1 1]
+                       [0 0 0 0 0]
+                       [2 2 2 2 2]])
+
+(def reelstrip [[0 1 2 3 4 5 6 7 8 9 10]
+                      [0 1 2 3 4 5 6 7 8 9 10]
+                      [0 1 2 3 4 5 6 7 8 9 10]
+                      [0 1 2 3 4 5 6 7 8 9 10]
+                      [0 1 2 3 4 5 6 7 8 9 10]])
+
+(defn make-symbolset [stop]
+     [(get (get reelstrip 0) (get stop 0))
+      (get (get reelstrip 1) (get stop 1))
+      (get (get reelstrip 2) (get stop 2))
+      (get (get reelstrip 3) (get stop 3))
+      (get (get reelstrip 4) (get stop 4))])
 
 (defn spin []
   (let [stop (vec (map rand-int(
-                         map count reelstrip)))]
+                         map count reelstrip)))
+         vs [[1 2 3 4 5]]]
     {:stop stop
-     :vs [[0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0]]
+;     :vs [[0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0]]
+      :vs (make-symbolset stop)
      :reward 9999}))
